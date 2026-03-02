e94d90d4e54adb95b36aacbe8916ccfa5ea7b72b
roeenangelo <roeenangelo@gmail.com>
2026-03-02 17:28:14 +0800

    Add split_nodes_delimiter and tests

---
+++ src/inline_markdown.py
@@ -0,0 +1,37 @@
+from textnode import TextNode, TextType
+import re
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        split_nodes = []
+        parts = old_node.text.split(delimiter)
+
+        if len(parts) % 2 == 0:
+            raise ValueError(f"Invalid Markdown: Missing closing delimiter '{delimiter}'")
+        
+        for i in range(len(parts)):
+            if parts[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(parts[i], text_type))
+
+        new_nodes.extend(split_nodes)
+    return new_nodes
+        
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_link(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
