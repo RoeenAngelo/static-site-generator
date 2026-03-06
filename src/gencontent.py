3fa75f9647c66f57e0b42e4a548262c5f4a21e82
roeenangelo <roeenangelo@gmail.com>
2026-03-06 22:09:33 +0800

    Refactor generate_pages_recursive

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,35 +1,16 @@
 import os
+from pathlib import Path
 from block_markdown import markdown_to_html_node
-
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     for filename in os.listdir(dir_path_content):
-        source_path = os.path.join(dir_path_content, filename)
+        from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-
-        if os.path.isfile(source_path):
-            if source_path.endswith("md"):
-                # change filename.md to filename.html
-                dest_path = dest_path.replace(".md", ".html")
-                with open(source_path, 'r') as f:
-                    markdown_content = f.read()
-                with open(template_path, 'r') as f:
-                    template_content = f.read()
-                html_str = markdown_to_html_node(markdown_content).to_html()
-                title = extract_title(markdown_content)
-
-                final_html = template_content.replace("{{ Title }}", title)
-                final_html = final_html.replace("{{ Content }}", html_str)
-
-                dest_dir = os.path.dirname(dest_path)
-
-                if dest_dir:
-                    os.makedirs(dest_dir, exist_ok=True)
-
-                with open(dest_path, 'w') as f:
-                    f.write(final_html)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
         else:
-            generate_pages_recursive(source_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
