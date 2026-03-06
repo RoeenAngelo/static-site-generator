a60dd037559409e125d950a17ef93f2e8eee002f
roeenangelo <roeenangelo@gmail.com>
2026-03-06 21:46:51 +0800

    Add generate_pages_recursive

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,6 +2,35 @@ import os
 from block_markdown import markdown_to_html_node
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        source_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+
+        if os.path.isfile(source_path):
+            if source_path.endswith("md"):
+                # change filename.md to filename.html
+                dest_path = dest_path.replace(".md", ".html")
+                with open(source_path, 'r') as f:
+                    markdown_content = f.read()
+                with open(template_path, 'r') as f:
+                    template_content = f.read()
+                html_str = markdown_to_html_node(markdown_content).to_html()
+                title = extract_title(markdown_content)
+
+                final_html = template_content.replace("{{ Title }}", title)
+                final_html = final_html.replace("{{ Content }}", html_str)
+
+                dest_dir = os.path.dirname(dest_path)
+
+                if dest_dir:
+                    os.makedirs(dest_dir, exist_ok=True)
+
+                with open(dest_path, 'w') as f:
+                    f.write(final_html)
+        else:
+            generate_pages_recursive(source_path, template_path, dest_path)
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as f:
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from copy_static import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_page, generate_pages_recursive 
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -17,11 +17,12 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
     print(f"Successfully copied all contents to '{dir_path_public}'.")
     print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    # generate_page(
+    #     os.path.join(dir_path_content, "index.md"),
+    #     template_path,
+    #     os.path.join(dir_path_public, "index.html"),
+    # )
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
